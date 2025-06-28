7067c8572e06ba2ca10e859e758a0d61397ad442
Jerison <jerisonw@gmail.com>
2025-06-28 15:05:28 -0700

    fixed compression path and recording logic

--- detector.py
+++ detector.py
@@ -1,5 +1,7 @@
 import psutil
 from recorder import record, websocket, disconnect
+import time
+
 
 def check_process(process_name):
     for proc in psutil.process_iter(['name']):
@@ -15,8 +17,16 @@ def check_process(process_name):
 
 def main():
     ws = websocket()
+    while not check_process("League of Legends.exe"):
+        print("Waiting")
+        time.sleep(2)
+    print("found")
+    record(ws)
     while check_process("League of Legends.exe"):
-        print("found")
-        record(ws)
+        time.sleep(1)
+    print("ending")
     disconnect(ws)
+
+if __name__ == "__main__":
+    main()
 
--- recorder.py
+++ recorder.py
@@ -1,11 +1,40 @@
 import obsws_python as obs
-import sys
-import time 
+import time
 from settings import OBS
+import subprocess
+import glob
+import os
 
+def compress_video(input_path, output_path, crf=28):
+    subprocess.run([
+        'ffmpeg',
+        '-i', input_path,
+        '-vcodec', 'libx264',
+        '-crf', str(crf),
+        output_path
+    ])
 
 def websocket():
     ws = obs.ReqClient(host="localhost", port=4455, password=OBS)
+    return ws
+
+def disconnect(ws):
+    ws.stop_record()
+    time.sleep(2)
+    record_status = ws.get_record_status()
+    print("record_status", dir(record_status))
+    record_path = ws.get_record_directory().record_directory
+
+    list_of_files = glob.glob(os.path.join(record_path, '*.mp4'))
+    list_of_files.sort(key=os.path.getmtime)
+
+    
+    most_recent = list_of_files[-1]
+
+    compressed_path = most_recent.replace(".mp4", "_compressed.mp4")
+    compress_video(most_recent, compressed_path)
+
+    ws.disconnect()
 
 def record(ws):
     try:
@@ -16,9 +45,4 @@ def record(ws):
         
 
     except KeyboardInterrupt:
-        pass
-
-
-def disconnect(ws):
-    ws.stop_record()
-    ws.disconnect()
+        disconnect(ws)
